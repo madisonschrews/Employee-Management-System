e7bc7f6ccb36fbdd8a9f7ce601502f0e5481607e
Madison Schrews <maddyschrews@gmail.com>
2020-05-05 11:15:55 -0400

    edits to sql file

--- seeds.sql
+++ seeds.sql
@@ -5,5 +5,5 @@ VALUES  ('Engineering'),
         ('Sales');
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES  ('Chris', 'Malilay', 1, 1),
+VALUES  ('Madison', 'Schrews', 1, 1),
         ('Test', 'Boi', 2, 1);
